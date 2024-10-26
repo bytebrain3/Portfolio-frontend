5d06ad85bd521db3e3812de996e4b6cc33964652
Dip mondal <work.dipmondal@gmail.com>
2024-10-26 18:42:19 +0600

    src/axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: 'https://portfolio-backend-8lef.onrender.com',
-	headers: {
-		'Content-Type': 'application/json',
-	},
-	// Enable sending cookies with requests
-	withCredentials: true,
+    baseURL: 'https://portfolio-backend-8lef.onrender.com',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true,
 });
 
 export default axiosInstance;
