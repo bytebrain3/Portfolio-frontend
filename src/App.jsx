9d3b0a9bb47e55ffe45ab4cdfc6523c402cf899c
Dip mondal <work.dipmondal@gmail.com>
2024-10-26 23:34:56 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="w-screen h-screen overflow-y-auto bg-transparent text-black dark:text-white transition-colors duration-500 font-poppins">
       <RouterView />
-
     </div>
   );
 }
